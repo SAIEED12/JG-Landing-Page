b6b2c2af67959f1132e5d922a2494a8bf835cdd1
SAIEED12 <abusaieed0@gmail.com>
2026-07-24 01:31:40 +0600

    Refactor code structure for improved readability and maintainability

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,12 @@ export default function Home() {
     <div>
       JG Home
       21-07-26
+      <Image
+        src="/KJG-01_jpg_0a4190d5-bd45-4dd8-be79-7df5b22fbcbb.webp"
+        width={400}
+        height={400}
+        alt="banner"
+      />
     </div>
   );
 }
